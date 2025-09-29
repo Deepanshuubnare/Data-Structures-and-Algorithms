fb31ac9355c6023b0600019285ffd797e70f0bc9
Deepanshuubnare <145673182+Deepanshuubnare@users.noreply.github.com>
2025-09-29 17:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (83.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -11,20 +11,23 @@ int f(vector<vector<int>>&grid,vector<vector<int>>&dp,int i,int j,int n,int m){
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
         // return f(grid,dp,n-1,m-1,n,m);
+        vector<int>prev(m,0);
+        vector<int>curr(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 && j==0) dp[i][j]=grid[0][0];
+                if(i==0 && j==0) curr[j]=grid[0][0];
                 else{
                     int top=1e8;
-                    if(i-1>=0) top=grid[i][j]+dp[i-1][j];
+                    if(i-1>=0) top=grid[i][j]+prev[j];
                     int left=1e8;
-                    if(j-1>=0) left=grid[i][j]+dp[i][j-1];
-                    dp[i][j]=min(top,left);
+                    if(j-1>=0) left=grid[i][j]+curr[j-1];
+                    curr[j]=min(top,left);
                 }
             }
+            prev=curr;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
